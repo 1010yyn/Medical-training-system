8607f084950f45b1f1b3c9811318d7758481c154
1010yyn <yangyiling1997@yeah.net>
2020-09-27 21:31:14 +0800

    课程添加环节

--- src/views/courseCentre/coursePage.vue
+++ src/views/courseCentre/coursePage.vue
@@ -10,7 +10,7 @@
       >
         <el-carousel-item
           v-for="item in courseList"
-          :key="item.pic"
+          :key="item"
         >
           <el-image
             class="image"
@@ -19,21 +19,60 @@
           />
         </el-carousel-item>
       </el-carousel>
+
+      <div
+        id="div1"
+        @drop="drop(event)"
+        @dragover="allowDrop(event)"
+      ></div>
+      <el-button
+        draggable="true"
+        type="primary"
+        @ratechange="drag(event)"
+      >
+        添加
+      </el-button>
+
+      <el-divider />
+      <div>
+        <el-button
+          class="add"
+          icon="el-icon-plus"
+          circle
+          @click="onFirstClick"
+        />
+        <li
+          v-for="item in list"
+          :key="item.name"
+          class="list"
+        >
+          <img
+            class="atmosphere"
+            :src="img"
+          >
+          <el-button
+            class="add"
+            icon="el-icon-plus"
+            circle
+            @click="onclick(item)"
+          />
+        </li>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
 import 'video.js/dist/video-js.css'
-// import pdf from 'vue-pdf'
 
 export default {
   name: 'CoursePage',
-  // components: { pdf },
   data() {
     return {
       title: 'CoursePage',
       course_id: '',
+      img: require('@/icons/img/head1.jpg'),
+      list: [{ name: 1 }, { name: 2 }, { name: 3 }],
       courseList: [require('@/sample/pic1.jpg'), require('@/sample/pic2.jpg'), require('@/sample/pic3.jpg'), require('@/sample/pic4.jpg'), require('@/sample/pic5.jpg'), require('@/sample/pic6.jpg'), require('@/sample/pic7.jpg'), require('@/sample/pic8.jpg'), require('@/sample/pic9.jpg'), require('@/sample/pic10.jpg'), require('@/sample/pic11.jpg')]
     }
   },
@@ -41,7 +80,35 @@ export default {
     this.course_id = this.$route.params
   },
   methods: {
-
+    onFirstClick() {
+      console.log('点击第一个')
+      var tmp = { name: 888 }// 在列表最前面添加一个元素
+      this.list.unshift(tmp)
+    },
+    onclick(item) {
+      console.log(item)
+      var index = this.list.indexOf(item)
+      var oldList = this.list.slice(0, index + 1)// 取当前添加位置及以前的list片
+      var newList = this.list.slice(index + 1, this.list.length)// 取当前添加位置以后的list片
+      console.log(oldList)
+      console.log('索引：' + index)
+      console.log(newList)
+      const newItem = this.list[0]
+      newItem.name = '324'// 一个测试
+      this.list = oldList.concat(newItem, newList) // 已合并的方式插入新的item
+      console.log(this.list)
+    },
+    allowDrop(ev) {
+      ev.preventDefault()
+    },
+    drag(ev) {
+      ev.dataTransfer.setData('Text', ev.target.id)
+    },
+    drop(ev) {
+      var data = ev.dataTransfer.getData('Text')
+      ev.target.appendChild(document.getElementById(data))
+      ev.preventDefault()
+    }
   }
 }
 </script>
@@ -59,9 +126,32 @@ export default {
     .class-centre-card {
       margin: 30px;
       min-height: 580px;
+      #div1 {
+        width: 350px;
+        height: 70px;
+        padding: 10px;
+        border: 1px solid #aaaaaa;
+      }
       .image {
         height: 100%;
         width: 100%;
+      }
+      .add {
+        float: left;
+        margin-top: 10px;
+        margin-right: 10px;
+      }
+      .list {
+        display: inline-flex;
+        list-style: none;
+        margin-bottom: 15px;
+        .atmosphere {
+          height: 60px;
+        }
+        .add {
+          float: left;
+          margin: 10px;
+        }
       }
     }
   }
