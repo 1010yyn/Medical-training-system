2d0dbbdb5f4eb98e88be8daf6a249dfd42c73b0d
1010yyn <yangyiling1997@yeah.net>
2020-11-16 16:08:26 +0800

    11.16 考试成绩导出

--- src/views/examManager/modifyResultPage.vue
+++ src/views/examManager/modifyResultPage.vue
@@ -2,6 +2,16 @@
   <div class="class-centre-container">
     <h1 class="class-centre-title">{{ title }}</h1>
     <div class="class-centre-table">
+      <el-button
+        type="primary"
+        @click="exit"
+      >返回</el-button>
+      <el-button
+        plain
+        type="success"
+        @click="handleDownload"
+        style="float:right"
+      >导出excel表格</el-button>
       <el-table
         style="width: 100%;padding-top: 15px;"
         :data="exam_result"
@@ -30,15 +40,10 @@
           align="center"
         >
           <template slot-scope="scope">
-            <!-- <el-button
-              plain
-              type="primary"
-              @click="handleModify(scope.$index)"
-            >查看</el-button> -->
             <el-button
               plain
               type="primary"
-              @click="handleModify(scope.$index)"
+              @click="handleModify(scope.row)"
             >修改</el-button>
             <el-button
               plain
@@ -48,25 +53,92 @@
           </template>
         </el-table-column>
       </el-table>
+      <h3>总人数：{{ exam_result.length }}</h3>
+      <h3> 平均分：{{ average }}</h3>
     </div>
   </div>
 </template>
 
 <script>
+import { parseTime } from '@/utils'
+
 export default {
   data() {
     return {
       title: '考试结果管理',
-      exam_result: [{ user_id: '1', user_name: '小王', exam_score: '88' }, { user_id: '2', user_name: '小杜', exam_score: '10' }]
+      flag: true,
+      exam_result: [{ user_id: '1', user_name: '小王', exam_score: 88 }, { user_id: '2', user_name: '小杜', exam_score: 10 }],
+      average: 0,
+      downloadLoading: false,
+      filename: '',
+      autoWidth: true,
+      bookType: 'xlsx',
+      list: [{ user_id: '1', user_name: '小王', exam_score: 88 }, { user_id: '2', user_name: '小杜', exam_score: 10 }]
     }
   },
+  created() {
+    var sum = 0
+    var i = 0
+    while (this.exam_result[i]) {
+      sum = sum + this.exam_result[i].exam_score
+      console.log(this.exam_result[i].exam_score)
+      i++
+    }
+    console.log(sum)
+    this.average = sum / Number(this.exam_result.length)
+    console.log(this.average)
+  },
   methods: {
-
-    handleModify(index) {
-      this.exam_result[index].exam_score = '100'
+    handleModify(row) {
+      this.$prompt('请输入修改后的成绩', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+        // inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+        // inputErrorMessage: '邮箱格式不正确'
+      }).then(({ value }) => {
+        row.exam_score = value
+        this.$message({
+          type: 'success',
+          message: '修改成功'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        })
+      })
     },
     handleDelete(index) {
       this.exam_result.splice(index, index + 1)
+    },
+    handleDownload() {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['工号', '姓名', '考试成绩']
+        const filterVal = ['user_id', 'user_name', 'exam_score']
+        const list = this.list
+        const data = this.formatJson(filterVal, list)
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: this.filename,
+          autoWidth: this.autoWidth,
+          bookType: this.bookType
+        })
+        this.downloadLoading = false
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    },
+    exit() {
+      this.$router.push({ name: 'ExamManager' })
     }
   }
 }
